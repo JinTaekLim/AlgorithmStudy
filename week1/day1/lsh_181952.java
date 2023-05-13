423bfa1ea236f3f46be53ff17e8e411c1b62be46
LeeSeungHyeon <ujm0524@naver.com>
2023-05-13 23:49:59 +0900

    week1 문제 출제 완료

---
+++ week1/day1/lsh_181952.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+import java.util.Scanner;
+
+public class lsh_181952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String result = sc.next();
+        System.out.println(result);
+    }
+}
