f78ba469e7ff3feae423ae9843dd8cdb5bd3061f
chop028 <chopf028@gmail.com>
2023-05-15 20:56:41 +0900

    Create LJH_01.java

---
+++ week1/day1/LJH_01.java
@@ -0,0 +1,12 @@
+package study_01_01;
+
+import java.util.Scanner;
+
+public class LJH_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
