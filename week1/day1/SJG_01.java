9a65a5a9bbc66e206cefb96fe78090dfc499fc1b
vlcl753 <hello.git.vlcl@naver.com>
2023-05-18 21:28:50 +0900

    1번 문제

---
+++ week1/day1/SJG_01.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+import java.util.Scanner;
+
+public class SJG_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
