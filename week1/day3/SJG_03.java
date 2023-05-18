502bb9bba1204aff091c7c01c6f919026c87cc0c
vlcl753 <hello.git.vlcl@naver.com>
2023-05-18 21:33:30 +0900

    update

---
+++ week1/day3/SJG_03.java
@@ -0,0 +1,23 @@
+package week1.day3;
+
+
+import java.util.Scanner;
+
+public class SJG_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        if (str.length()>=1 && str.length()<=10)  {
+            if (n>=1 && n<=5){
+                int i = 0;
+                while(i<n){
+                    System.out.print(str);
+                    i++;
+                }
+
+            }
+        }
+    }
+}
