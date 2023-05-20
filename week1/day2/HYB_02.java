c7f79751ede189b2a8f3de4a3d6425836a86e4da
vlcl753 <hello.git.vlcl@naver.com>
2023-05-20 16:53:11 +0900

    Class명 수정

--- week1/day2/HYB_02.java
+++ week1/day2/HYB_02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class HYB_02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
