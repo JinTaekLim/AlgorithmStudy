2af98a7b84f6de0b2f9c3d04cb7b09a1c11b58bc
vlcl753 <hello.git.vlcl@naver.com>
2023-05-20 16:53:58 +0900

    Update

---
+++ week1/day4/SJG_04.java
@@ -0,0 +1,22 @@
+package week1.day4;
+import java.util.Scanner;
+
+public class SJG_04 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isLowerCase(ch)) {
+                ch = Character.toUpperCase(ch);
+            } else {
+                ch = Character.toLowerCase(ch);
+            }
+            System.out.print(ch);
+        }
+
+    }
+}
