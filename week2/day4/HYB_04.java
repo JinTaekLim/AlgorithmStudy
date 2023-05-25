18bdf9ea2c98c98d55e72b0f44d45d882dc52306
yyyubn <89472700+yyyubn@users.noreply.github.com>
2023-05-26 00:08:39 +0900

    Create HYB_04.java

---
+++ week2/day4/HYB_04.java
@@ -0,0 +1,14 @@
+package week2.day4;
+import java.util.Scanner;
+
+public class HYB_04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if(n%2==0)
+            System.out.print(n+" is even");
+        else
+            System.out.print(n+" is odd");
+    }
+}
