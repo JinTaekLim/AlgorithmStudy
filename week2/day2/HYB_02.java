3624aae8d1de1b84127b9fdf0f2f7d113924e5db
yyyubn <89472700+yyyubn@users.noreply.github.com>
2023-05-26 00:06:19 +0900

    Create HYB_02.java

---
+++ week2/day2/HYB_02.java
@@ -0,0 +1,11 @@
+package week2.day1;
+import java.util.Scanner;
+
+public class HYB_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.print(a+b);
+    }
+}
