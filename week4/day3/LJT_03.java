6aaef95bdca9edf97152cfc2c3b7b9761f51a2e2
JinTaekLim <jintaek019@gmail.com>
2023-06-07 22:27:24 +0900

    week4 LJT_03 추가

---
+++ week4/day3/LJT_03.java
@@ -0,0 +1,30 @@
+package week4.day3;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int A = 0;
+        int B = 0;
+
+        for(int i=1; i<n+1; i++){
+            if(i%2 == 0) A = A + i*i;
+            if(i%2 == 1) B += i;
+        }
+
+        if(n%2 == 0) answer = A;
+        if(n%2 == 1) answer = B;
+        return answer;
+    }
+}
+
+/*
+public class LJT_03 {
+    public static void main(String[] args) {
+        Solution p = new Solution();
+        System.out.print(p.solution(10));
+
+
+    }
+}
+
+ */
