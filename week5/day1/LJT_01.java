3e2e5a025828131d98abf3f7f5c6bf8488963885
JinTaekLim <75293102+JinTaekLim@users.noreply.github.com>
2023-06-12 22:20:13 +0900

    week5 LJT_01 추가

---
+++ week5/day1/LJT_01.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+
+        int mode = 0;
+        String ret = "";
+
+        for (int idx = 0; idx < code.length(); idx++) {
+
+
+            if (mode==0 && code.charAt(idx) == '1') mode = 1;
+            else if(mode==1 && code.charAt(idx) == '1') mode = 0;
+
+            if (mode == 0 && code.charAt(idx) != '1') {
+                if (idx % 2 == 0) {
+                    ret += code.charAt(idx);
+
+                }
+            }
+
+
+            if (mode == 1 && code.charAt(idx) != '1') {
+                if (idx % 2 != 0) {
+                    ret += code.charAt(idx);
+             }
+            }
+
+
+            System.out.println("idx = "+idx+"   code["+idx+"] = "+ code.charAt(idx) +"    mode = " + mode + "    ret = "+ret);
+        }
+
+        answer = ret;
+
+        if(answer==""){
+            answer = "EMPTY";
+        }
+
+        return answer;
+    }
+}
+
+public class test {
+    public static void main(String[] args) {
+        Solution p = new Solution();
+        System.out.print(p.solution("abc1abc1abc"));
+
+
+    }
+}
