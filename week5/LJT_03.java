1d661cc48bd9bccac1d71c44d20a0ced2450ce56
JinTaekLim <75293102+JinTaekLim@users.noreply.github.com>
2023-06-14 15:18:56 +0900

    week5 LJT_04 추가

---
+++ week5/LJT_03.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+
+        answer = a + b + c;
+
+        if(a==b || b==c || a==c){
+            answer *= (a*a + b*b + c*c);
+
+            if(a==b && b==c && a==c) {
+                answer *= (a * a * a + b * b * b + c * c * c);
+            }
+        }
+
+        return answer;
+    }
+}
