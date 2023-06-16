dc648576ecabc8849b36156564653e05587cc9f7
yyyubn <89472700+yyyubn@users.noreply.github.com>
2023-06-16 15:45:17 +0900

    Update HYB_05.java

--- week5/day5/HYB_05.java
+++ week5/day5/HYB_05.java
@@ -1,5 +1,25 @@
 package week5.day5;
 
 public class HYB_05 {
+  public String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
+        String temp="";
+        Map<String, Integer> playersMap = new HashMap<>();
+        
+        for(int i=0; i<players.length; i++)
+            playersMap.put(players[i], i+1);
+        
+        for(int i=0; i<callings.length; i++) {
+			 temp = players[playersMap.get(callings[i])-2];
+			 players[playersMap.get(callings[i])-2] = players[playersMap.get(callings[i])-1];
+			 players[playersMap.get(callings[i])-1] = temp;
+			 
+			 playersMap.put(callings[i], playersMap.get(callings[i])-1);
+			 playersMap.put(temp, playersMap.get(temp)+1);
+			 
+		}
+        
+        return players;
+    }
 
 }
