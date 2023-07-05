6aac58b8dce4728ef70b8303b35c8bece9145796
vlcl753 <hello.git.vlcl@naver.com>
2023-07-05 23:56:32 +0900

    Update

---
+++ week7/day1/SJG_01.java
@@ -0,0 +1,14 @@
+package week7.day1;
+
+public class SJG_01 {
+    public static int[] solution(int[] arr, int[][] queries) {
+        for (int[] query : queries) {
+            int i = query[0];
+            int j = query[1];
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        return arr;
+    }
+}
