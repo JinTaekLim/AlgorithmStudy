8bda51c8ab2e8c582d8804ed6d9962e71844b5b7
JinTaekLim <75293102+JinTaekLim@users.noreply.github.com>
2023-07-06 16:14:19 +0900

    week7 LJT_03 추가

---
+++ week7/day3/LJT_03.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+
+        for(int i=0;i<queries.length;i++){
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+
+            for(int j=s;j<=e;j++){
+                if(j%k==0){
+                    arr[j] += 1;
+                }
+            }
+        }
+
+        answer = arr;
+
+        return answer;
+    }
+}
