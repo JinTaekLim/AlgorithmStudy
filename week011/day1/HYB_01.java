ac2f59cf2f0d79cffbea48d085efe1a841a5195f
yyyubn <89472700+yyyubn@users.noreply.github.com>
2023-08-15 17:28:14 +0900

    HYB_01 수정

--- week011/day1/HYB_01.java
+++ week011/day1/HYB_01.java
@@ -4,7 +4,7 @@ public class HYB_01 {
     public String solution(String my_string, int n) {
         String answer = "";
         int cnt=my_string.length();
-        answer=my_string.substring(cnt-n);
+        answer=my_string.substring(cnt-n); //뒤에서부터 n글자니까 (문자열 길이-n)해서 몇번째부터 문자열 잘라야되는지 확인
         return answer;
     }
 }
